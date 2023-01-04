f1622412392063efefd3257aa6aaaa1af2602b73
Sascha Gärtner <sascha.gaertner@salex.org>
2023-01-04 20:12:53 +0100

    adding tests

---
+++ src/test/java/org/salex/hmip/client/test/TestTest.java
@@ -0,0 +1,11 @@
+package org.salex.hmip.client.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+    @Test
+    void should_test_something() {
+        Assertions.assertTrue(true);
+    }
+}
